7db89b8ec4789741cbfee72e39dde94c80ff9c2a
Swapnashree Sahoo <163818083+swapna-shree@users.noreply.github.com>
2025-06-11 21:40:58 +0530

    Added Currency Converter
    1- Made use of custom hooks
    2- Import/Export through Components
    3- Fetch using IDs from JSON instead of using keys
    4- Use a unique and stable identifier, like id from data.
        Avoid using index as key, unless:
        -The list is static,
        -There’s no unique identifier,
        -Order and items never change

---
+++ 03-currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("Fetched:", res[currency]);
+        setData(res[currency]);
+      })
+      .catch((err) => console.error("Error fetching currency info:", err));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
